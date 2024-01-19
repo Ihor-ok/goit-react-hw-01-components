e53283054927a8684cae49ec64bf1b9cd63be5db
Ihor <kovaluk_igor@ukr.net>
2024-01-19 23:13:38 +0100

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
+import Profile from 'components/Profile/Profile.jsx'
+import Statistics from 'components/Statistics/Statistics.jsx'
+import user from '../user.json'
+import data from '../data.json'
+
+// { avatar,  username, tag, location, stats}
+
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+ <>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data}  />
+
+      {/* <div>
+        <Statistics title="Upload stats"  />
+    
+        {data.map(d => (<Statistics
+        key={d.id}
+        statsLabel={d.label}
+        statsPercentage={d.percentage}
+        />))}
+      </div> */}
+    </>
+  )
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import css from "./Statistics.module.css";
+
+export default function Statistics({ title, stats }) {
+  //    const defaultPercentage = 0;
+  
+
+
+    return (
+      <section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
+        {stats.map(dataStats => {
+          return (<ul className={css.stat_list}>
+            <li className={css.item}>
+            <span className={css.label}>{dataStats.label}</span>
+              <span className={css.percentage}>{dataStats.percentage }</span>
+            </li>
+          </ul>)
+        })}
+  
+        
+    {/* <ul className="stat-list">
+    <li className="item">
+        <span className="label">{stats[1].label}</span>
+      <span className="percentage">{stats[1].percentage }</span>
+    </li> */}
+    {/* <li className="item">
+      <span className="label">{stats.label}</span>
+      <span className="percentage">{stats.percentage ?? defaultPercentage}</span>
+    </li>
+    <li className="item">
+      <span className="label">{stats.label}</span>
+      <span className="percentage">{stats.percentage ?? defaultPercentage}</span>
+    </li>
+    <li className="item">
+      <span className="label">{stats.label}</span>
+      <span className="percentage">{stats.percentage ?? defaultPercentage}</span>
+    </li> */}
+  {/* </ul> */}
+</section>
+    )
+
+ }
+
+// Statistics.propTypes = {
+//     title: PropTypes.string,
+//     stats: PropTypes.object,
+
+// }
